fc50738e9a2a66b25fb98c35bf68fe40cccab5ae
Bukola Adegbembo <developerbukola@gmail.com>
2022-08-06 13:20:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ const submit = document.querySelector('.submit');
 const hidden = document.querySelector('.hidden')
 const modal = document.querySelector('.modal')
 const container = document.querySelector('.container')
-const one = document.querySelector('.one');
+const btnEl = document.querySelectorAll('.btn');
+const rate = document.querySelectorAll('.rate')
 
 
 submit.addEventListener('click',function(){
   modal.classList.remove('hidden');
   container.style.display = 'none'
-
-
 })
 
-one.addEventListener('click', function() {
-  one.style.backgroundColor = '#7c8798'
-  
-})
+
+
 
 
 
